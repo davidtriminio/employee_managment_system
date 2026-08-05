afd9811d9f50451df9b227f90e50ed137b393392
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-04 22:20:55 -0600

    feat(layout): implement topbar and sidebar

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NgClass
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  isSidebarExpanded: boolean = true
+
+  toggleSidebar(): void {
+    this.isSidebarExpanded = !this.isSidebarExpanded
+  }
+}
