001752b1e89644a056d5298062467fa0806a3df8
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-04 22:46:47 -0600

    feat: complete layout using localstorage and route

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 import {NgClass} from '@angular/common';
+import {IUser} from '../../core/model/interfaces/User.Model';
+import {GlobalConstant} from '../../core/globalConstant/Global.constant';
 
 @Component({
   selector: 'app-layout',
@@ -13,6 +15,20 @@ import {NgClass} from '@angular/common';
 })
 export class Layout {
   isSidebarExpanded: boolean = true
+  loggedUserData!: IUser
+  router = inject(Router)
+
+  constructor() {
+    const localData = localStorage.getItem(GlobalConstant.LOGIN_LOCAL_KEY)
+    if (localData != null){
+      this.loggedUserData = JSON.parse(localData)
+    }
+  }
+
+  onLogOff(){
+    localStorage.removeItem(GlobalConstant.LOGIN_LOCAL_KEY)
+    this.router.navigate(['login'])
+  }
 
   toggleSidebar(): void {
     this.isSidebarExpanded = !this.isSidebarExpanded
