a21ab0e77d011e3c513c008cf55bdf3cf90c8853
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-05 22:03:17 -0600

    feat: add user Model

--- src/app/core/model/interfaces/User.Model.ts
+++ src/app/core/model/interfaces/User.Model.ts
@@ -9,3 +9,21 @@
   role: string
   createdDate: string
 }
+
+export interface IApiResponseModel {
+  message: string,
+  result: boolean,
+  data: any
+}
+
+export interface IParentDept {
+  departmentId: number,
+  departmentName: string,
+  departmentLogo: string
+}
+export interface IChildDept {
+  childDeptId: number
+  parentDeptId: number
+  departmentName: string
+}
+
