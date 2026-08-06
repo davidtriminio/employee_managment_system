d8e9a59a1d7017f5aef2e7e852ea4515177cc2c0
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-05 22:38:06 -0600

    feat: add employee form call

--- src/app/pages/employee-form/employee-form.ts
+++ src/app/pages/employee-form/employee-form.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {EmployeeModel} from '../../core/model/classes/Employee.model';
+import {EmployeeService} from '../../core/services/employee-service';
+import {MasterService} from '../../core/services/master-service';
+import {IApiResponseModel, IChildDept, IParentDept} from '../../core/model/interfaces/User.Model';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css',
 })
-export class EmployeeForm {}
+export class EmployeeForm implements OnInit {
+  employeeObj: EmployeeModel = new EmployeeModel()
+  empSrv = inject(EmployeeService)
+  masterSrv = inject(MasterService)
+
+  parentDeptList: WritableSignal<IParentDept[]> = signal([])
+  childDeptList: WritableSignal<IChildDept[]> = signal([])
+
+  ngOnInit(): void {
+    this.getParentDept()
+  }
+
+  getParentDept() {
+    this.masterSrv.getAllParentDept().subscribe({
+      next: (res:IApiResponseModel) => {
+        this.parentDeptList.set(res.data)
+      }
+    })
+  }
+
+  onChangeParent(event: any){
+    const id= event.target.value
+    this.masterSrv.getAllChildDeptByParentId(id).subscribe({
+      next:(res:IApiResponseModel) =>{
+        this.childDeptList.set(res.data)
+      }
+    })
+  }
+
+  onSaveEmp() {
+    this.empSrv.onCreateEmployee(this.employeeObj).subscribe({
+      next: (res: EmployeeModel) => {
+        alert("Employee Created Success")
+      },
+    })
+  }
+}
