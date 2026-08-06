b15e44b11f9065d2560ed110f5ae0cd7978c2417
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-05 22:07:14 -0600

    feat: add API_METHOD constants

--- src/app/core/globalConstant/Global.constant.ts
+++ src/app/core/globalConstant/Global.constant.ts
@@ -1,3 +1,7 @@
 ﻿export const GlobalConstant = {
-    LOGIN_LOCAL_KEY: 'emp_user'
+  LOGIN_LOCAL_KEY: 'emp_user',
+  API_METHOD: {
+    GET_ALL_EMPLOYEE: 'GetAllEmployees',
+    CREATE_EMPLOYEE: 'CreateEmployee'
+  }
 }
