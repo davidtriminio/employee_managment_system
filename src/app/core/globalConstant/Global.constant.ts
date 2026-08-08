033a1f595621903b229cb5ea39697c6a891dc746
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-07 20:12:58 -0600

    feat: add projects constants

--- src/app/core/globalConstant/Global.constant.ts
+++ src/app/core/globalConstant/Global.constant.ts
@@ -3,6 +3,8 @@
   API_METHOD: {
     GET_ALL_EMPLOYEE: 'GetAllEmployees',
     GET_EMPLOYEE_BY_ID: 'GetEmployee/',
-    CREATE_EMPLOYEE: 'CreateEmployee'
+    CREATE_EMPLOYEE: 'CreateEmployee',
+    GET_ALL_PROJECTS: 'GetAllProjects',
+    CREATE_NEW_PROJECT: 'CreateProject'
   }
 }
