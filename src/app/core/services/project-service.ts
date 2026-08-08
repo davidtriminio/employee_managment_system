ad007b714555edc4019433d1b419e577704ae00f
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-07 20:13:19 -0600

    feat: add project service

---
+++ src/app/core/services/project-service.ts
@@ -0,0 +1,21 @@
+import {inject, Service} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {GlobalConstant} from '../globalConstant/Global.constant';
+import {Observable} from 'rxjs';
+import {IProject, NewProjectModel} from '../model/interfaces/User.Model';
+
+@Service()
+export class ProjectService {
+
+  http = inject(HttpClient)
+
+  getAllProjects(): Observable<IProject[]> {
+    return this.http.get<IProject[]>(environment.API_URL + GlobalConstant.API_METHOD.GET_ALL_PROJECTS)
+  }
+
+  createProject(obj: NewProjectModel): Observable<NewProjectModel>{
+    return this.http.post<NewProjectModel>(environment.API_URL + GlobalConstant.API_METHOD.CREATE_NEW_PROJECT, obj)
+  }
+
+}
