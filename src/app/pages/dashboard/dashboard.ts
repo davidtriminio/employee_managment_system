f725f5810d371fa63beb930ce8ad342576b5eb77
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-08-08 09:03:23 -0600

    feat: finish dashboard API implementation

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {MasterService} from '../../core/services/master-service';
+import {IDashboard} from '../../core/model/interfaces/User.Model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  masterService = inject(MasterService)
+  dashboardData = signal<IDashboard | null>(null)
+  isLoading = signal<boolean>(false)
+  errorMessage = signal<string>('')
+
+  ngOnInit() {
+    this.loadDashboard()
+  }
+
+  loadDashboard() {
+    this.isLoading.set(true)
+    this.errorMessage.set('')
+
+    this.masterService.getDashboard().subscribe({
+      next: (res: IDashboard) => {
+        this.dashboardData.set(res)
+        this.isLoading.set(false)
+      },
+      error: () => {
+        this.errorMessage.set('No se pudo cargar la informacion del dashboard.')
+        this.isLoading.set(false)
+      }
+    })
+  }
+}
